ec497b01ad5f9839d4385acc6323bf6510a91d5c
melonmochi <melonmochi3@gmail.com>
2020-12-11 18:58:13 +0100

    chore: test env variables

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,17 @@
 import { FC, StrictMode } from 'react';
 
-const App: FC = () => (
-  <StrictMode>
-    <div>{process.env.MAPBOX_GL_TOKEN}</div>
-  </StrictMode>
-);
+const App: FC = () => {
+  console.log(
+    'im process.env',
+    process.env.CUSTOM,
+    process.env.NODE_ENV,
+    process.env.MAPBOX_GL_TOKEN
+  );
+  return (
+    <StrictMode>
+      <div>{process.env.MAPBOX_GL_TOKEN}</div>
+    </StrictMode>
+  );
+};
 
 export default App;
